ecd5c9bac9deadb7290ea3177b88f88e596d091f
prashantKR2503 <prashantkumar2503@gmail.com>
2024-08-29 15:12:40 +0530

    Update q3.py

--- Week3/q3.py
+++ Week3/q3.py
@@ -2,13 +2,5 @@ text = input("Enter the string: ")
 
 length = len(text)
 num = 0
-if length%2 == 0:
-    num1 = (length-1)//2
-    for i in range(0, num1-2):
-        print(text[num])
-        num += 2
-else:
-    num1 = (length + 1) // 2
-    for i in range(0, num1):
-        print(text[num], end='')
-        num += 2
+for i in range(0, length, 2):
+    print(text[i], end="")
